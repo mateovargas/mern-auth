136a76c28cae2626dd197d4797e3b6a944093911
Mateo Vargas <mateovargas5@gmail.com>
2025-11-28 00:21:15 -0800

    Fixing broken routing

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(errorHandler);
 if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
     app.use(express.static(path.join(__dirname, 'frontend/dist')));
-    app.get(/.*/, (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+    app.get('/*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
 } else {
     app.get('/', (req, res) => res.send('Server is ready'));
 }
