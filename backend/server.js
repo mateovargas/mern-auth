d2ca2397d3256bacfc9c93745998c6b1de7164f2
Mateo Vargas <mateovargas5@gmail.com>
2025-11-28 00:24:36 -0800

    moving error handling

--- backend/server.js
+++ backend/server.js
@@ -23,17 +23,18 @@ app.use(cookieParser());
 
 //API Routes
 app.use('/api/users', userRoutes);
-app.use(notFound);
-app.use(errorHandler);
 
 //production set up or test
 if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
     app.use(express.static(path.join(__dirname, 'frontend/dist')));
-    app.get('/*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+    app.get(/.*/, (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
 } else {
     app.get('/', (req, res) => res.send('Server is ready'));
 }
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server started on PORT: ${port}`);
